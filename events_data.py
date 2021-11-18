2c94642cb835b804aa7b5ca5b0064bf7de5ba09d
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-11-18 09:48:00 +0530

    event list

---
+++ events_data.py
@@ -0,0 +1,93 @@
+events = [
+    {
+        'title': 'Artificial Intelligence in Healthcare',
+        'content': '- Speaker: Padmini Janaki - Position: CEO at Mind and Mom - Date: 18th April 2021 - Time: 11: 30 am onwards ()',
+        'poster': 'https: //firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2Fai%20in%20healthcare.png?alt=media&token=dbf2392a-6930-496d-9dbf-f9c9d06597a7',
+        'youtube': 'https://youtu.be/KKCNwLIF5RA',
+        'link': 'https://bit.ly/AI_HC ',
+        'open': False
+    },
+
+
+    {
+        'open': False,
+        'youtube': '-',
+        'title': 'Introduction to\nSemiconductor\nand IC Industry',
+        'poster': 'https: //firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2Fsemiconductor%20and%20ic%20industry.png?alt=media&token=2d62c2d3-4b78-47f8-8edd-3d891691cde8',
+        'content': '- Speaker: Chahat Ahuja - Position: GPU Design Verification Engineer - Date: 30th April 2021 - Time: 7:30 pm onwards ()',
+        'link': 'https://t.ly/7wBU '
+    },
+
+
+    {
+        'poster': 'https: //firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2Fgpt3.png?alt=media&token=7405e61c-7dd5-4733-83d7-99fd5f2f54f2',
+        'content': '- Speaker: Andrew Mayne - Position: Creative Applications at OpenAI - Date: 6th May 2021 -Time: 11:00 AM Onwards',
+        'title': 'GPT-3 by OpenAI',
+        'open': False,
+        'youtube': 'https://youtu.be/ob5VllUBdWo',
+        'link': 'https://bit.ly/OpenAI_workshop'
+    },
+
+    {
+        'youtube': 'https://youtu.be/s7VWx5QGyWU',
+        'content': '- Speaker: Sharath Cherian Thomas - Position: Certified Google Cloud Engineer - Date: 14th May 2021 -Time: 4:00 PM Onwards',
+        'open': False,
+        'poster': 'https://firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2Fdevops.png?alt=media&token=4adcd52e-64f9-4c5c-91ce-f03c7b0947dd',
+        'link': '-',
+        'title': 'DevOps'
+    },
+
+    {
+        'youtube': 'https://youtu.be/nRd6Mo_ibe8',
+        'poster': 'https://firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2Fcloud%20computing%20with%20aws.png?alt=media&token=ceacb98c-3aff-40b5-abee-2e438efe2791',
+        'title': 'Introduction to Cloud\nComputing with AWS',
+        'open': False,
+        'content': '- Speaker: Harsh Bardhan Mishra - Position: Country Head Script Foundation - Date: 15th May 2021 - Time: 7:00 PM Onwards',
+        'link': '-'
+    },
+
+    {
+        'link': '-',
+        'title': 'Internal Design-a-thon',
+        'open': False,
+        'youtube': '-',
+        'content': 'A well-deserved hearty congratulations to the ISA Design-a-thon Winner Team Pixelated and the Runner up Chaos! Well done all the participants: Team Bold Creatives, Fury and XD, ',
+        'poster': 'https: //firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2Finternal%20designathon.jpeg?alt=media&token=9818c266-987c-4cee-a251-393706a8b1bd'
+    },
+
+    {
+        'link': 'https://bit.ly/IoT_Session',
+        'open': False,
+        'poster': 'https: //firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2FDSW_IoT_1.png?alt=media&token=e6ac43b1-f809-4777-9100-396f00b897ef',
+        'youtube': 'https://www.youtube.com/watch?v=I_2gnl9MgKg',
+        'title': 'Impact of Cloud Computing in IoT',
+        'content': 'Speaker name : Saravanan Ganesan  Designation : Senior Engineer - IoT Date  : 11.08.2021 (Wednesday) Time  : 7:30pm'
+    },
+
+    {
+        'content': 'We will venture into the world of Printed Circuit Board Designing through Eagle CAD software!',
+        'link': 'https://meet.google.com/sdt-ntjk-ydo',
+        'youtube': 'https://www.youtube.com/watch?v=RGThP20xQeU',
+        'title': 'Introduction to PCB Design and Electronic Design Automation',
+        'open': False,
+        'poster': 'https://firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2FIntroduction%20to%20PCB%20Design%20and%20Electronic%20Design%20Automation.jpeg?alt=media&token=7c4d1699-33e5-40d2-a89b-a92ecaa92d37'
+    },
+
+    {
+        'open': False,
+        'poster': 'https://firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2FCloudAppwrite.png?alt=media&token=4a3dc4f0-44cf-4a19-bad0-3e0efd2e00bb',
+        'link': '-',
+        'title': 'Cloud Computing Simplified using Microsoft Azure',
+        'content': 'ISA-VIT is organizing a session with Mr. Aditya Oberai, an MLH Coach & an Engineer at Appwrite!',
+        'youtube': 'https://www.youtube.com/watch?v=cDPY7tVuUNY'
+    },
+
+    {
+        'poster': 'https: //firebasestorage.googleapis.com/v0/b/isa-vit.appspot.com/o/Technitudes%2FNumpyPandas.png?alt=media&token=667f7cfb-8bfa-48c6-b798-b5befadc3179',
+        'open': False,
+        'title': 'Data Science Series, Technitude-1',
+        'content': 'We are excited to bring to you this cracking series of technitudes that will launch you into the awesome world of data science with Python!',
+        'youtube': 'https://youtu.be/t4jjZPTF8jE',
+        'link': 'https://meet.google.com/qaf-serk-nnw'
+    }
+]
--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+from events_data import events
+
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+result = db.collection(u'Events').stream()
+
+
+print(events)
